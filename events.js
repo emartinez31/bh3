53da761ad5420b5e73b830955462a205373b7e98
rcastro2 <rcastro@baysidehighschool.org>
2014-09-01 07:45:30 -0400

    prototyped the events page

---
+++ events.js
@@ -0,0 +1,157 @@
+{
+	"events":[
+		{
+		    "id" :15,
+			"title": "Open Sew at the Castle",
+			"image": "queensSewing.jpg",
+			"date": "Saturday September 13, 2014",
+			"time" : "1pm - 4pm",
+			"cost" : "$8 BHS members; $10 non-member",
+			"abstract": "Do you quilt, sew, or just love fabric and textiles? Or maybe you would you like to learn? Join other sewing enthusiasts for an afternoon of creating and socializing.",
+			"description":"<p>Do you quilt, sew, or just love fabric and textiles? Or maybe you would you like to learn more about it? Join other sewing enthusiasts for an afternoon of creating and socializing.</p><p><br/>This program is supported, in part, by public funds from the New York City Department of Cultural Affairs in partnership with the City Council.</span></p><p><img src='images/DCAlogo2.jpg'/></p>"
+		},
+		{
+			"id" :14,
+			"title": "Bayside Day! Launch Party",
+			"image": "urban.jpg",
+			"date": "",
+			"time": "",
+			"cost" : "BHS Members: $10; bring a guest: $25 per person",
+			"abstract": "",
+			"description":"<p>Guest Speaker: Cindy Lobel, food historian and author of <i>Urban Appetites: Food and Culture in 19th Century New York</i></p><p>1914 inspired appetizers and drinks will be served</p><p>BHS Members Only Cocktail Reception </p><p>Reservations required: 718.352.1548</p><p><br/>This program is supported, in part, by public funds from the New York City Department of Cultural Affairs in partnership with the City Council.</span></p><p><img src='images/DCAlogo2.jpg' /></p>"
+		},
+		{
+			"id" :13,
+			"title": "Bayside Day Commemoration and Festival",
+			"image": "Bayside Day Web.jpg",
+			"date": "Saturday September 27, 2014",
+			"time": "1pm - 5pm",
+			"cost" : "Free",
+			"abstract": "",
+			"description":"<p>Come celebrate the 100th anniversary of Bayside Day!<br/>Vintage Automobile Parade! Entertainment! Beer and Wine Garden! Food Tent! Vintage Games and Challenges! Bring Your Own Bike (BYPB) to decorate and enter the velocipede parade! Rain or Shine. </p><p>Parking available within Fort Totten on designated streets.</p><p><br/>This program is supported, in part, by public funds from the New York City Department of Cultural Affairs in partnership with the City Council.</p><p><img src='images/DCAlogo2.jpg' /></p>"
+		},
+		{
+			"id" :12,
+			"title": "2014 New York Archives Week",
+			"image": "NY Archives Week_Logo_2014.jpg",
+			"date": "October 5 - 11, 2014",
+			"time": "7pm - 9pm",
+			"cost" : "Free",
+			"abstract": "",
+			"description":"<p>The Archivists Round Table of Metropolitan New York, Inc .(A.R.T.), along with hundreds of organizations in the archives community across New York State, will celebrate the 26th Anniversary of New York Archives Week, October 5-11, 2014, with special commemorative activities throughout the New York City Metropolitan area.  New York Archives Week is an annual celebration aimed at informing the general public of the diverse array of archival materials available in the Metropolitan New York City region.</p>"
+		},
+		{
+			"id" :11,
+			"title": "12th Annual Open House New York Weekend",
+			"image": "OHNY_website logo.png",
+			"date": "Saturday October 11, 2014<br/>Sunday October 12, 2014",
+			"time": "1pm - 3pm",
+			"cost" : "Free",
+			"abstract": "",
+			"description":"<p>Open House New York weekend is a five-borough-wide event that celebrates architecture and design. This year, Open House New York takes place throughout New York City on October 11th and 12th. </p>"
+		},
+		{
+			"id" :10,
+			"title": "A Day at the Races: Second Annual Bayside Trifecta",
+			"image": "Belmont-Race-Track.jpg",
+			"date": "Thursday October 16, 2014",
+			"time": "12pm - 5pm",
+			"cost" : "",
+			"abstract": "",
+			"description":"<p>Belmont Park Race Track at the Turf and Field Club.</p><p>Co-Sponsored with the Bayside Business Association</p><p><a href='http://baysideba.com' target='_blank'>reservations and additional information</a></p>"
+		},
+		{
+			"id" :9,
+			"title": "Pop-Up Birthday Card Workshop",
+			"image": "paper-scissors-glue.jpg",
+			"date": "Call to reserve",
+			"time": "Call to reserve",
+			"cost" : "BHS Member: $15; non-member: $10",
+			"abstract": "",
+			"description":"Join BHS instructor, Janet Epstein who will teach you how to create unique hand-crafted pop-up birthday cards using cut-out paper and folds. This workshop is suitable for all ages, with or without experience. All materials will be provided.<p>Call to reserve: 718.352.1548"
+		},
+		{
+			"id" :8,
+			"title": "13th Annual Totten Trot Foot Race, Fun Run and 5K Halloween Hustle",
+			"image": "totten-trot-1.jpg",
+			"date": "Sunday October 26, 2014",
+			"time": "Check-in: 7:30 Start: 9am",
+			"cost" : "$30 through 10/15, $35 10/16 through race-day",
+			"abstract": "Bright colors, tranparency, and light: this is what glass painting is all about. Spend some playful timne together making a colorful sun-catcher! The class will introduce you to the basics of glass painting.",
+			"description":"<p>Register online through NYCRuns</p><p><a href='https://nycruns.com/races/?race=totten-trot-5k-foot-race' target='_blank'><img src='images/nyc_run.jpeg' width='217' height='217' border='0' /></a></p><div>Course: Waterfront course in historic Fort Totten Park &ndash; a pristine stretch surrounding a preserved Civil War fortress on Little Neck Bay in Bayside. Runners will pass through the historic district which consists of buildings dating to the late 1800s. The Fort is situated where the Long Island Sound meets the East River at the Throgs Neck Bridge. The start and the finish are located at the landmark Officers&rsquo; Club, home of the Bayside Historical Society. The course includes a loop out onto the Joe Michaels Mile along Little Neck Bay. The course will be USATF-certified and NYCRUNS will provide professional scoring and race results.</div><p> A  Course Map can be found <a href='images/totten_trot_course_map.pdf' target='_blank'>here.</a></p><p>Cost: $30 through 10/15, $35 10/16 through race-day.</p><p>Kids&rsquo; Fun Run: Free at 10 a.m. (costumes suggested) a short-distance sprint for tiny tots and kids up to 12 years of age, grouped in age categories.  Special &ldquo;COOKIE MEDALS&rdquo; for participants! </p><div>Prizes: Cash prizes will go to the overall male and female winners of the 5K 1st Place&ndash; $75 2nd Place&ndash; $50 3rd Place&ndash; $25 Awards will be given to the male and female winners of each of the following divisions: 14 and under; 15-19; 20-29; 30-39; 40-49; 50-59; 60-69; 70-79; 80+Also, a special prize valued at $50 will go to the best costume and stick around after the race to try your luck at the $50 raffle prize and many great gift certificate raffles!!</div><p>&nbsp;</p><p>Packet Pickup: Packet pickup and race-day registration will begin at 7:30am at the Castle.</p><div>   Unique to the Totten Trot: After the race, runners are invited to the Halloween-themed ballroom of the landmark Officers&rsquo; Club of BHS. The award ceremony and raffle drawings will then take place. The fun continues with a fall continental breakfast and live music to keep the spirits flying. Rumor has it that some ghouls, goblins and witches will make a scary appearance and antique cars and trucks will be on display. Join the fun!! T-Shirts: Designer t-shirts guaranteed to all participants who register on or before 10/15. Goodie Bags: Great complimentary gifts and coupons from our sponsors and local merchants.</div><p>&nbsp;</p><div> Transportation and Parking: The race site is easily accessible by public transportation and convenient to travel by car.  Parking will be available to participants, entrance is through the Main Gate at 212th Street and Cross Island Parkway. </div><p>PLEASE ARRIVE EARLY!! Questions: Call 718-352-1548 or visit www.baysidehistorical.org</p>"
+		},
+		{
+			"id" :7,
+			"title": "Glass Painting Workshop",
+			"image": "glassPaint.jpg",
+			"date": "Saturday August 3, 2014",
+			"time": "12pm - 2pm",
+			"cost" : "$25",
+			"abstract": "Bright colors, tranparency, and light: this is what glass painting is all about. Spend some playful timne together making a colorful sun-catcher! The class will introduce you to the basics of glass painting.",
+			"description":"Bright colors, tranparency, and light: this is what glass painting is all about. Spend some playful timne together making a colorful sun-catcher! The class will introduce you to the basics of glass painting. <br/>You will learn how to trace a design, color it with transparent, water-based paints and make a shiny and joyful decoration to hang at the window.<br/>Reservations required: 718.352.1548"
+		},
+		{
+			"id" :6,
+			"title": "Jazz Brunch at the Castle",
+			"image": "piano_keys.jpg",
+			"date": "Sunday July 27, 2014",
+			"time" : "Noon - 2pm",
+			"cost" : "$20 BHS members; $25 non-members",
+			"abstract": "Beat the heat with some cool jazz.! Enjoy light refreshments while listening to our jazz trio.",
+			"description":"Beat the heat with some cool jazz.! Enjoy light refreshments while listening to our jazz trio. Pre-paid reservations required by Friday, July 25, 2014<br/>718.352.1548"
+		},
+		{
+			"id" :5,
+			"title": "Croquet Day",
+			"image": "Winslow_Homer_-_Croquet_Players.jpg",
+			"date": "Saturday July 19, 2014",
+			"time" : "4pm",
+			"cost" : "$10",
+			"abstract": "Join us for a delightful afternoon croquet party including light refreshments and seasonal libations. Equipment and instructions will be provided. As is customary, please wear your best white sporting attire.",
+			"description":"Join us for a delightful afternoon croquet party including light refreshments and seasonal libations. Equipment and instructions will be provided. As is customary, please wear your best white sporting attire. <br/>Reservations suggested: 718.352.1548"
+		},
+		{
+			"id" :4,
+			"title": "'A Spirited Evening' at the Castle",
+			"image": "castle side.jpg",
+			"date": "Friday July 18, 2014",
+			"time" : "7pm - 9pm",
+			"cost" : "$5 BHS memnbers; $8 non-members",
+			"abstract": "Join the Seekers Club for a special evening investigation of the c.1887 castle. Follow along as 'The Seekers' explore dark corners using the latest technology in the field of paranormal investigation.",
+			"description":"Join the Seekers Club for a special evening investigation of the c.1887 castle. Follow along as 'The Seekers' explore dark corners using the latest technology in the field of paranormal investigation. RSVP 718.352.1548"
+		},
+		{
+			"id" :3,
+			"title": "The Business of Bayside",
+			"image": "Gidon Pesso of Pesso's IcesX.jpg",
+			"date": "Friday July 11, 2014",
+			"time" : "7pm",
+			"cost" : "Free",
+			"abstract": "As part of the 100th anniversary commemorating Bayside Day 1914, BHS has organized an exhibit featuring photographs of local business owners taken by community members.",
+			"description":"The 100th Anniversary of Bayside Day takes place this year. The original event in 1914 was sponsored by local business leaders, civic associations, elected officials, and Bayside residents. The idea behind it was to promote civic pride, community spirit, and boost local businesses. The original celebration featured five parades, a barbeque for 3,000 attendees, several baking contests and an inaugural ball for the elected 'Queen of Bayside Day'.As part of the 100th anniversary commemorating Bayside Day 1914, BHS has organized an exhibit featuring photographs of local business owners taken by community members. Above photo: Gidon Pesso, owner of Pesso's Italian Ice at 203-20 35th Avenue, Bayside NY by Shawn King."
+		},
+		{
+			"id" :2,
+			"title": "Open Sew at the Castle",
+			"image": "queensSewing.jpg",
+			"date": "Saturday June 28, 2014<br/>Saturday July 12, 2014<br/>Saturday August 3, 2014",
+			"time" : "1pm - 4pm",
+			"cost" : "$8 BHS members; $10 non-member",
+			"abstract": "Do you quilt, sew, or just love fabric and textiles? Or maybe you would you like to learn? Join other sewing enthusiasts for an afternoon of creating and socializing.",
+			"description":"<p>Do you quilt, sew, or just love fabric and textiles? Or maybe you would you like to learn more about it? Join other sewing enthusiasts for an afternoon of creating and socializing. Co-sponsored by Queens Sewing Social.</p><p><br/>This program is supported, in part, by public funds from the New York City Department of Cultural Affairs in partnership with the City Council.</span></p><p><img src='images/DCAlogo2.jpg'/></p>"
+		},
+		{
+			"id" :1,
+			"title": "BHS Annual Lawn Concert",
+			"image": "something_in_between.jpg",
+			"date": "Sunday June 22, 2014",
+			"time" : "6pm - 8pm",
+			"cost" : "Free",
+			"abstract": "Free outdoor concert featuring &quot;Something In Between&quot;, playing all of the hits from the 60s through today. Attendees are welcome to bring lawn chairs, blankets, and picnics. Located on the sledding hill at Crocheron Park.",
+			"description":"Free outdoor concert featuring 'Something In Between', playing all of the hits from the 60s through today. Attendees are welcome to bring lawn chairs, blankets, and picnics.Located on the sledding hill at Crocheron Park.<br/>For more information about the band view their <a href='http://www.something-inbetween.com/'>site</a>.<br/>Co-sponsored by New York City Department of Parks and Recreation."
+		}
+
+		
+	]
+	
+}
